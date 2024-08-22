14d5cecfaa6d7b5906f4ef2ea85c571b1506d5b5
Micaela <myaque@dc.uba.ar>
2024-08-22 00:50:08 -0300

    refactor: Refactor max2 and normaVectorial functions in p1/ej1.hs

---
+++ p1/ej2.hs
@@ -0,0 +1,8 @@
+--Ejercicio 2 ⋆
+--i. Definir la función curry, que dada una función de dos argumentos, devuelve su equivalente currificada.
+--ii. Definir la función uncurry, que dada una función currificada de dos argumentos, devuelve su versión no
+--currificada equivalente. Es la inversa de la anterior.
+--iii. ¿Se podría definir una función curryN, que tome una función de un número arbitrario de argumentos y
+--devuelva su versión currificada?
+--Sugerencia: pensar cuál sería el tipo de la función.
+
