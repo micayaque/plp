3bd4e0071a1d19297cc8fc83d46ff0919637b093
Micaela <myaque@dc.uba.ar>
2024-08-22 00:29:07 -0300

    refactor: Refactor max2 and normaVectorial functions in p1/ej1.hs

--- p1/ej1.hs
+++ p1/ej1.hs
@@ -9,9 +9,17 @@
 max2 :: (Float, Float) -> Float
 max2 (x, y) | x >= y = x
             | otherwise = y
+--no esta currificada
+--version currificada
+max2' :: Float -> Float -> Float
+max2' x y = if x >= y then x else y
 
 normaVectorial :: (Float, Float) -> Float
 normaVectorial (x, y) = sqrt (x^2 + y^2)
+--no esta currificada
+--version currificada
+normaVectorial' :: Float -> Float -> Float
+normaVectorial' x y = sqrt (x^2 + y^2)
 
 subtract2 :: Float -> Float -> Float
 subtract2 = flip (-)
