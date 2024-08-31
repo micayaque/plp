2384cff7f8e7e6928528f88df2771e831bf92f08
Micaela <myaque@dc.uba.ar>
2024-08-31 02:26:40 -0300

    add ej 4

--- p1/ej4.hs
+++ p1/ej4.hs
@@ -17,13 +17,19 @@ permutaciones = foldr (concatMap . agregarEnTodasLasPosiciones) [[]]
 --mismos elementos de L, en su mismo orden de aparición. 
 --Ejemplo: partes [5, 1, 2] → [[], [5], [1], [2], [5, 1], [5, 2], [1, 2], [5, 1, 2]](en algún orden).
 
-
+partes :: [a] -> [[a]]
+partes = foldr (\x res -> res ++ map (x:) res) [[]]
 
 --iii. Definir la función prefijos, que dada una lista, devuelve todos sus prefijos.
 --Ejemplo: prefijos [5, 1, 2] → [[], [5], [5, 1], [5, 1, 2]].
 
-
+prefijos :: [a] -> [[a]]
+prefijos = foldr (\x res -> []:map (x:) res) [[]]
+--prefijos xs = [take i xs | i <- [0..(length xs)]]
 
 --iv. Definir la función sublistas que, dada una lista, devuelve todas sus sublistas (listas de elementos que aparecen 
 --consecutivos en la lista original).
 --Ejemplo: sublistas [5, 1, 2] → [[], [5], [1], [2], [5, 1], [1, 2], [5, 1, 2]] (en algún orden).
+
+sublistas :: [a] -> [[a]]
+sublistas xs = [] : [ take j (drop i xs) | i<-[0..(length xs)] , j<-[1.. length xs -i]]
